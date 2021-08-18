a0c8ee791da8c444f160b1f7f7de5aa6bd25bce2
juosta <79445286+juosta@users.noreply.github.com>
2021-08-18 20:05:08 +0300

    data annotations

--- Models/DTO/CreateCompanyDTO.cs
+++ Models/DTO/CreateCompanyDTO.cs
@@ -6,11 +6,15 @@ namespace Notificationschedulingsystem.Models.DTO
 {
     public class CreateCompanyDTO
     {
+        [Key]
         public Guid Id { get; set; }
         public string CompanyName { get; set; }
+        [Required]
         [RegularExpression("[0-9]{10}", ErrorMessage = "Entered company number format is not valid.")]
         public string CompanyNumber { get; set; }
+        [Required]
         public CompanyTipe Type { get; set; }
+        [Required]
         public Market Market { get; set; }
     }
 }
